2da855203c2f04524f7fca5232c8bad9e7e4e8ed
edsonibarra <edsonibarra97@gmail.com>
2024-09-10 13:18:56 -0600

    Binary Tree initial implementation

--- binary_tree.py
+++ binary_tree.py
@@ -1,5 +1,23 @@
+# Implementation of a binary tree node
 class Node:
     def __init__(self, value):
         self.value = value
-        self.left = None
-        self.right = None
+        self.left = None # The left child of the node
+        self.right = None # The right child of the node
+
+
+# Implementation of a Binary Tree
+class BinaryTree:
+    def __init__(self, root):
+        self.root = Node(root)
+
+
+# Add nodes to the Binary Tree
+def main():
+    tree = BinaryTree(1)
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
+    tree.root.right.left = Node(6)
+    tree.root.right.right = Node(7)
