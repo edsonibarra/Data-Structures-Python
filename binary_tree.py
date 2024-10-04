4cc74b1e689d83aed660a8a0b9d1c4ffaa8e5d93
edsonibarra <edsonibarra97@gmail.com>
2024-10-03 23:53:44 -0600

    in-order and post-order traversal methods

--- binary_tree.py
+++ binary_tree.py
@@ -18,10 +18,51 @@ class BinaryTree:
         self.root = Node(root)
 
     def preorder_print(self, start: Node, traversal: str):
+        """
+        Root->Left->Right
+        """
         if start:
             traversal += str(start.value) + "-"
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+    def in_order_print(self, start: Node, traversal: str) -> str:
+        """
+        In-Order Traversal Algorithm:
+
+        Left->Root->Right
+
+        1. Check if the current node is empty/None.
+        2. Traverse the left subtree by recursively calling
+        the in-order method.
+        3. Display the data part of the root (or the current node).
+        4. Traverse the right subtree by recursively calling
+        the in-order method.
+        """
+        if start:
+            traversal =  self.in_order_print(start.left, traversal)
+            traversal += str(start.value) + "-"
+            traversal = self.in_order_print(start.right, traversal)
+        return traversal
+    
+    def post_order_print(self, start: Node, traversal: str) -> str:
+        """
+        Post-Order Traversal Algorithm:
+
+        Left->Right->Root
+
+        1. Check if the current node is empty/None.
+        2. Traverse the left subtree by recursively calling
+        the post-order method.
+        3. Traverse the right subtree by recusively calling
+        the post-order method.
+        4. Display the data part of the root.
+        """
+        if start:
+            traversal = self.post_order_print(start.left, traversal)
+            traversal = self.post_order_print(start.right, traversal)
+            traversal += str(start.value) + "-"
         return traversal
 
 
